244cafbca57827032050e561c09cc0e717d9585c
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2015-04-13 14:55:12 +0200

    Updated logging code in the frontcontroller to not respond to localhost

--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -7,11 +7,16 @@ class Index extends Controller
 {
 	public function getIndex()
 	{
-		$log = new \App\Models\Log();
-		$log->ip = $_SERVER['REMOTE_ADDR'];
-		$log->url = $_SERVER['REQUEST_URI'];
-		$log->timestamp = timestamp('Y-m-d H:i:s');
-		$log->create();
+		$dontlog = array('::1', '127.0.0.1');
+		
+		if(!in_array($_SERVER['REMOTE_ADDR'], $dontlog))
+		{
+			$log = new \App\Models\Log();
+			$log->ip = $_SERVER['REMOTE_ADDR'];
+			$log->url = $_SERVER['REQUEST_URI'];
+			$log->timestamp = timestamp('Y-m-d H:i:s');
+			$log->create();
+		}
 
 		$attributes = array(
 			'textDemoLink' => url('TextDemo', 'getIndex')
