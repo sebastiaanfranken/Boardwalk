84ad40cc4443901ea2ecf0e25d33803263b78b48
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2015-04-13 19:10:18 +0200

    Added master before method in the base controller to generate test data

--- App/Controllers/Controller.php
+++ App/Controllers/Controller.php
@@ -4,4 +4,19 @@ namespace App\Controllers;
 abstract class Controller
 {
 	public function __construct(){}
+
+	/**
+	 * This is a "master" before function. It doesn't matter which child controller you're using, this
+	 * function _will_ get called.
+	 *
+	 * Useful for a query, or a log
+	 */
+	public function before()
+	{
+		$log = new \App\Models\Log();
+		$log->ip = $_SERVER['REMOTE_ADDR'];
+		$log->url = $_SERVER['REQUEST_URI'];
+		$log->timestamp = timestamp('Y-m-d H:i:s');
+		$log->create();
+	}
 }
--- App/Controllers/Index.php
+++ App/Controllers/Index.php
@@ -7,17 +7,6 @@ class Index extends Controller
 {
 	public function getIndex()
 	{
-		$dontlog = array('::1', '127.0.0.1');
-
-		if(!in_array($_SERVER['REMOTE_ADDR'], $dontlog))
-		{
-			$log = new \App\Models\Log();
-			$log->ip = $_SERVER['REMOTE_ADDR'];
-			$log->url = $_SERVER['REQUEST_URI'];
-			$log->timestamp = timestamp('Y-m-d H:i:s');
-			$log->create();
-		}
-
 		$attributes = array(
 			'textDemoLink' => url('TextDemo', 'getIndex'),
 			'databaseDemoLink' => url('DatabaseDemo', 'getIndex')
