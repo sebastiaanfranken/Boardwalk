c413f1f1c82250d9786ef52923b09a3eb633bf68
Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
2015-04-19 00:35:58 +0200

    Added 1st version of ACL, do _not_ use in production. Ever

---
+++ vendor/boardwalk/src/Boardwalk/ACL/ACL.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Main ACL package
+ * @author Sebastiaan Franken <sebastiaan@sebastiaanfranken.nl>
+ * @package Boardwalk\ACL
+ * @todo Fix this package. Do _not_ use
+ */
+
+namespace Boardwalk\ACL;
+
+use Exception;
+
+class ACL
+{
+
+}
